4535f19ebcfb6e8433145e6debeb4f70b817227e
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2020-11-04 13:36:05 +0100

    Create Project

---
+++ HTMLParser/src/htlgrieskirchen/po3/tsturm18/Tag.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htlgrieskirchen.po3.tsturm18;
+
+/**
+ *
+ * @author timst
+ */
+public class Tag {
+
+    private String value;
+    private String fullStartTag;
+
+    private String tag;
+    private String content;
+
+    public Tag(String value) {
+
+    }
+
+}
